ae59b749dce150cf7fac197e2bf8a604befe6208
Ellipsis-Fr <crespel.romain@gmail.com>
2023-04-01 15:40:13 +0200

    File Structue
    Made it possible to launch different executables
    Added a common method of reading puzzles

--- src/test/test.rs
+++ src/test/test.rs
@@ -1,8 +1,8 @@
-mod text_file_reader;
-use text_file_reader::TextFileReader;
+use AoC_2022::text_file_reader::{self, TextFileReader};
 
 
 fn main() {
+    println!("LANCE DEPUIS TEST.RS");
     // Example to use text_file_reader
     let mut text_file_reader = TextFileReader::new("test.txt");
     if let Ok(()) = text_file_reader.read_file_text() {
---
+++ src/text_file_reader/mod.rs
@@ -0,0 +1,31 @@
+use std::env;
+
+use std::error::Error;
+use std::fs;
+
+const PATH_TO_PUZZLE: &str = "puzzles/";
+
+pub struct TextFileReader {
+    file_name: String,
+    content: Option<String>,
+}
+
+impl TextFileReader {
+    pub fn new(file_name: &str) -> TextFileReader {
+        TextFileReader {
+            file_name: file_name.to_string(),
+            content: None
+        }
+    }
+
+    pub fn read_file_text(&mut self) -> Result<(), Box<dyn Error>> {
+        let file_location = PATH_TO_PUZZLE.to_owned() + &self.file_name;
+        println!("localisation du fichier : {file_location}");
+        self.content = Some(fs::read_to_string(file_location)?);
+        Ok(())
+    }
+
+    pub fn get_content(&self) -> &str {
+        &self.content.as_ref().unwrap()
+    }
+}

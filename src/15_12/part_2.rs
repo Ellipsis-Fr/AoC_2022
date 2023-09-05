72062d54adbf3bb45ee907fcd0839b4497aa594b
Ellipsis-Fr <crespel.romain@gmail.com>
2023-09-05 21:07:45 +0200

    15_12_p2 finished

--- src/15_12/part_2.rs
+++ src/15_12/part_2.rs
@@ -11,14 +11,14 @@ fn main() {
     
     let puzzle = get_puzzle();
     let sensors = get_sensors(puzzle);
-    
-    let limit_x = (0, 20);
-    let limit_y = (0, 20);
+
+    let limit_x = (0, 4_000_000);
+    let limit_y = (0, 4_000_000);
 
     let distress_beacon_coordinates = get_distress_beacon_coordinates(&sensors, limit_x, limit_y);
-    if distress_beacon_coordinates.is_some() {
-        let distress_beacon_coordinates = distress_beacon_coordinates.unwrap();
-        println!("Distress beacon coordinates : ({:?}, {:?}),\nTuning frequency : {:?}", distress_beacon_coordinates.0, distress_beacon_coordinates.1, distress_beacon_coordinates.0 * 4_000_000 + distress_beacon_coordinates.1);
+    match distress_beacon_coordinates {
+        Some(d) => println!("Distress beacon coordinates : ({:?}, {:?}),\nTuning frequency : {:?}", d.0, d.1, d.0 * 4_000_000 + d.1),
+        None => println!("No coordinates found")
     }
 }
 
@@ -39,38 +39,53 @@ fn get_sensors(puzzle: Vec<String>) -> Vec<Sensor> {
 }
 
 fn get_distress_beacon_coordinates(sensors: &Vec<Sensor>, limits_x: (i64, i64), limits_y: (i64, i64)) -> Option<Point> {
-    let dx = 1 + limits_x.1 - limits_x.0;
-    let dy = 1 + limits_y.1 - limits_y.0;
+    let mut distress_beacon_coordinates = None;
+    const STEP_AWAY: i64 = 1;
     
-    // Map
-    let rows = vec!['.'; dx as usize];
-    let mut map = vec![rows; dy as usize];
+    'row: for y in limits_y.0..=limits_y.1 {
+        let mut next_x = limits_x.0;        
+        'column: loop {
+            if next_x > limits_x.1 {
+                break;
+            }
+            let x = next_x;
 
-    for sensor in sensors {
-        let circle = sensor.get_position().get_all_point_from_a_perimeter(sensor.get_distance());
-
-        for coordinate in circle {
-            let x = coordinate.0;
-            let y = coordinate.1;
-
-            if x < limits_x.0 || x > limits_x.1 || y < limits_y.0 || y > limits_y.1 {
-                continue;
+            for sensor in sensors {
+                let sensor_position = sensor.get_position();
+                let radius = sensor.get_distance();
+                let manhattan_distance = sensor_position.get_manhattan_distance(&Point(x, y));
+                
+                if manhattan_distance > radius {
+                    continue;
+                } else if manhattan_distance == radius {
+                    if x >= sensor_position.0 {
+                        next_x = STEP_AWAY + x;
+                        continue 'column;
+                    } else {
+                        let dx = (sensor_position.0 - x).abs();
+                        let dy = (sensor_position.1 - y).abs();
+                        next_x = STEP_AWAY + x + dx + radius - dy;
+                        continue 'column;
+                    }
+                } else {
+                    if x >= sensor_position.0 {
+                        let dx = (sensor_position.0 - x).abs();
+                        let dy = (sensor_position.1 - y).abs();
+                        next_x = STEP_AWAY + x + radius - (dx + dy);
+                        continue 'column;
+                    } else {
+                        let dx = (sensor_position.0 - x).abs();
+                        let dy = (sensor_position.1 - y).abs();
+                        next_x = STEP_AWAY + x + dx + radius - dy;
+                        continue 'column;
+                    }
+                }
             }
 
-            // let a = map.get_mut(y as usize).unwrap().get_mut(x as usize).unwrap();
-            // *a = 'X';
-            *(map.get_mut(y as usize).unwrap().get_mut(x as usize).unwrap()) = 'X';
-        }
-        println!("Capteur fait");
-    }
-
-    let mut distress_beacon_coordinates = None;
-    for (y, row) in map.iter().enumerate() {
-        if row.contains(&'.') {
-            let x = row.iter().position(|&r| r == '.').unwrap();
-            distress_beacon_coordinates = Some(Point(x as i64, y as i64));
+            distress_beacon_coordinates = Some(Point(x, y));
+            break 'row;
         }
     }
-
+    
     distress_beacon_coordinates
 }
